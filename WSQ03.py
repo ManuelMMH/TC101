e8e36a606d8dd3bcd7627e437d939f8a0ff08001
ManuelMMH <manuelmendezhijar@gmail.com>
2016-02-10 07:42:47 -0700

    WSQ03

---
+++ WSQ03.py
@@ -0,0 +1,7 @@
+a=int(input("Give me a number: "))
+b=int(input("Give me another number: "))
+
+print (a, "-" ,b, "=" ,a - b)
+print (a, "*" ,b, "=" ,a * b)
+print (a, "/" ,b, "=" ,a // b)
+print ("The remainder of the integer division between",a, "and" ,b, "is =" , a % b)
