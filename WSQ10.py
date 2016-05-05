f02c8c9f3556aa0829fdb4b13610043adbe7a096
ManuelMMH <manuelmendezhijar@gmail.com>
2016-05-05 06:52:12 -0500

    WSQ10

---
+++ WSQ10.py
@@ -0,0 +1,9 @@
+from statistics import *
+
+list = [float(input("1st number: ")), float(input("2nd number: ")), float(input("3rd number: ")), float(input("4th number: ")),
+        float(input("5th number: ")), float(input("6th number: ")), float(input("7th number: ")), float(input("8th number: ")),
+        float(input("9th number: ")), float(input("10th number: "))]
+
+print ("Total: ", sum(list))
+print ("Standart deviation: ",stdev (list))
+print ("Average deviation: ", pstdev (list))
